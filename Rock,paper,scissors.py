ac09e5f46cb6577f60093e8e2087213cfe51e9f7
AnanyaSinha2717 <ananyasinha2717@gmail.com>
2023-06-29 13:02:40 +0530

    include while loop

--- Rock,paper,scissors.py
+++ Rock,paper,scissors.py
@@ -1,64 +1,72 @@
 import random
-q=input("Choose rock,paper or scissors ")
-rock='''
-    _______
----'   ____)
-      (_____)
-      (_____)
-      (____)
----.__(___)⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
-'''
-paper='''
-    _______
----'   ____)____
-          ______)
-          _______)
-         _______)
----.__________)
-'''
-scissors='''
-    _______
----'   ____)____
-          ______)
-       __________)
-      (____)
----.__(___)
-'''
-if q=="rock":
-    print(rock)
-elif q=="paper":
-    print(paper)
-else:
-    print(scissors)
+end=False
+while end==False:
+    q=input("Choose rock(1),paper(2) or scissors(3): ")
+    rock='''
+        _______
+    ---'   ____)
+          (_____)
+          (_____)
+          (____)
+    ---.__(___)⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+    '''
+    paper='''
+        _______
+    ---'   ____)____
+              ______)
+              _______)
+             _______)
+    ---.__________)
+    '''
+    scissors='''
+        _______
+    ---'   ____)____
+              ______)
+           __________)
+          (____)
+    ---.__(___)
+    '''
+    if q==1:
+        print(f"You chose: rock {rock}")
+    elif q==2:
+        print(f"You chose: paper {paper}")
+    else:
+        print(f"You chose: scissors {scissors}")
 
-x=random.randint(0,2)
-if x==0:
-    print(f"computer chose rock: {rock}")
-elif x==1:
-    print(f"computer chose paper: {paper}")
-elif x==2:
-    print(f"computer chose scissors: {scissors}")
+    x=random.randint(0,2)
+    if x==0:
+        print(f"Computer chose: rock {rock}")
+    elif x==1:
+        print(f"Computer chose: paper {paper}")
+    elif x==2:
+        print(f"Computer chose: scissors {scissors}")
 
-if q=="rock":
-    if x==0:
-        print("tie")
-    elif x==1:
-        print("you lost")
-    elif x==2:
-        print("you won")
-elif q=="paper":
-    if x==0:
-        print("you won")
-    elif x==1:
-        print("tie")
-    elif x==2:
-        print("you lost")
-else:
-    if x==0:
-        print("you lost")
-    elif x==1:
-        print("you won")
-    elif x==2:
-        print("tie")
+    if q==1:
+        if x==0:
+            print("Tie\n")
+        elif x==1:
+            print("You lost\n")
+        elif x==2:
+            print("You won\n")
+    elif q==2:
+        if x==0:
+            print("You won\n")
+        elif x==1:
+            print("Tie\n")
+        elif x==2:
+            print("You lost\n")
+    else:
+        if x==0:
+            print("You lost\n")
+        elif x==1:
+            print("You won\n")
+        elif x==2:
+            print("Tie\n")
 
-a=input("press enter to close")
+    ask=input("Do you want to continue? y/n: ").lower()
+    if ask=='y':
+        end=False
+    else:
+        end=True
+
+
