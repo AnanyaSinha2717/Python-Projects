c9a138c8382898b77ee1eb1e561538cc56aa36e0
AnanyaSinha2717 <ananyasinha2717@gmail.com>
2023-07-05 21:37:00 +0530

    upload my python projects

---
+++ coffee machine.py
@@ -0,0 +1,17 @@
+from coffee_data import MENU
+from coffee_data import resources
+def money():
+    q=int(input("How many quarters?: "))
+    d=int(input("How many dimes?: "))
+    n=int(input("How many nickels?: "))
+    p=int(input("How many pennies?: "))
+    total=q+d+n+p
+    print(f"You paid: {total}")
+def suff():
+    for i in resources:
+        if resources[i]>=
+print("WELCOME TO THE CAFE")
+ask=input("What do you wanna order? ('latte', 'espresso', 'cappuccino')")
+profit=0
+if ask=='latte':
+    MENU[1]
