2d67b19518f940012d03772abf8f8477de45d174
AnanyaSinha2717 <ananyasinha2717@gmail.com>
2023-07-07 22:02:56 +0530

    upload my python projects
    modifications

--- coffee machine.py
+++ coffee machine.py
@@ -1,19 +1,70 @@
 #initial
 
-from coffee_data import MENU
-from coffee_data import resources
-def money():
-    q=int(input("How many quarters?: "))
-    d=int(input("How many dimes?: "))
-    n=int(input("How many nickels?: "))
-    p=int(input("How many pennies?: "))
-    total=q+d+n+p
-    print(f"You paid: {total}")
-def suff():
-    for i in resources:
-        if resources[i]>=
+MENU = {"espresso": {"ingredients": {"water": 50, "coffee": 18,},"cost": 1.5,},
+        
+        "latte": {"ingredients": {"water": 200, "milk": 150,"coffee": 24,},"cost": 2.5,},
+        
+        "cappuccino": {"ingredients": {"water": 250,"milk": 100,"coffee": 24,},"cost": 3.0,}
+        }
+
+resources = {
+    "water": 300,
+    "milk": 200,
+    "coffee": 100,
+}
+
 print("WELCOME TO THE CAFE")
-ask=input("What do you wanna order? ('latte', 'espresso', 'cappuccino')")
-profit=0
-if ask=='latte':
-    MENU[1]
+
+#money
+def money(ask):
+    profit=0
+    t=int(input("How many quarters?: "))*0.25
+    t+=int(input("How many dimes?: "))*0.10
+    t+=int(input("How many nickels?: "))*0.05
+    t+=int(input("How many pennies?: "))*0.01
+    print(f"You paid: ${round(t,2)}")
+   
+    if t>MENU[ask]['cost']:
+        change=t-MENU[ask]['cost']
+        print(f"Here is ${round(change,2)} in change.")
+        profit+=MENU[ask]['cost']
+        print(f"Here is your {ask} ☕. Enjoy!")
+        
+    elif t==MENU[ask]['cost']:
+        profit+=MENU[ask]['cost'] 
+        print(f"Here is your {ask} ☕. Enjoy!")
+        
+    elif t<MENU[ask]['cost']:
+        print(f"Sorry, that's not enough money. Money refunded.")
+        return False
+    return True
+ 
+#order
+def order(ask):
+    for i in MENU[ask]['ingredients']:
+        if resources[i]>=MENU[ask]['ingredients'][i]:
+            resources[i]=resources[i]-MENU[ask]['ingredients'][i]
+            print(f"{i}={resources[i]}")
+            money(ask)
+            
+        elif resources[i]<MENU[ask]['ingredients'][i]:
+            print(f"Sorry, not enough {i}.")
+            return False
+    return True
+        
+            
+on=True
+while on:
+    ask=input("What do you wanna order? ('latte', 'espresso', 'cappuccino')").lower()
+    if ask=='report':
+        print(f"water = {resources[water]}")
+        print(f"milk = {resources[milk]}")
+        print(f"coffee = {resources[coffee]}")
+    elif ask=='off':
+        on=False
+    else:
+        order(ask)
+
+
+
+    
