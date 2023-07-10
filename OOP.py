de1d04e5a9cfce6dcc5bc1320f5e8596ffb4313a
AnanyaSinha2717 <ananyasinha2717@gmail.com>
2023-07-10 17:02:26 +0530

    upload my python projects

--- OOP.py
+++ OOP.py
@@ -1,6 +1,10 @@
 from turtle import Turtle,Screen
 t=Turtle()
 print(t)
+t.shape("turtle")
+t.color('DarkSeaGreen4')
+t.forward(199)
 my=Screen()
-print(my.canvheight)
+print(my.canvwidth)
 my.exitonclick()
+
