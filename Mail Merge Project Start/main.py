aa5266f7b6632856a77c4d8a4cb8fae86e2a48b3
AnanyaSinha2717 <ananyasinha2717@gmail.com>
2023-09-01 18:31:41 +0530

    upload my python projects

--- Mail Merge Project Start/main.py
+++ Mail Merge Project Start/main.py
@@ -1,18 +1,18 @@
-#TODO: Create a letter using starting_letter.txt 
-#for each name in invited_names.txt
-#Replace the [name] placeholder with the actual name.
-#Save the letters in the folder "ReadyToSend".
-    
-#Hint1: This method will help you: https://www.w3schools.com/python/ref_file_readlines.asp
-    #Hint2: This method will also help you: https://www.w3schools.com/python/ref_string_replace.asp
-        #Hint3: THis method will help you: https://www.w3schools.com/python/ref_string_strip.asp
+# TODO: Create a letter using starting_letter.txt
+# for each name in invited_names.txt
+# Replace the [name] placeholder with the actual name.
+# Save the letters in the folder "ReadyToSend".
+
+# Hint1: This method will help you: https://www.w3schools.com/python/ref_file_readlines.asp
+# Hint2: This method will also help you: https://www.w3schools.com/python/ref_string_replace.asp
+# Hint3: THis method will help you: https://www.w3schools.com/python/ref_string_strip.asp
 
 
 with open('./Input/Names/invited_names.txt', 'r') as file1:
     names = file1.readlines()
     for i in range(len(names)):
         name = names[i].strip()
-        names[i] = name
+        names[i] = name  # list of names created
 
     with open('./Input/Letters/starting_letter.txt', 'r+') as file2:
         content = file2.read()
@@ -20,4 +20,3 @@ with open('./Input/Names/invited_names.txt', 'r') as file1:
             with open(f'./Output/ReadyToSend/letter_for_{i}', 'w') as final:
                 letter = content.replace('[name]', i)
                 final.write(letter)
-                print(letter)
