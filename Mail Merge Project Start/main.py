eb2f69d9fecd23e17fdf3e8ff0b57b1461621cc9
AnanyaSinha2717 <ananyasinha2717@gmail.com>
2023-09-01 18:23:03 +0530

    upload my python projects

--- Mail Merge Project Start/main.py
+++ Mail Merge Project Start/main.py
@@ -6,3 +6,18 @@
 #Hint1: This method will help you: https://www.w3schools.com/python/ref_file_readlines.asp
     #Hint2: This method will also help you: https://www.w3schools.com/python/ref_string_replace.asp
         #Hint3: THis method will help you: https://www.w3schools.com/python/ref_string_strip.asp
+
+
+with open('./Input/Names/invited_names.txt', 'r') as file1:
+    names = file1.readlines()
+    for i in range(len(names)):
+        name = names[i].strip()
+        names[i] = name
+
+    with open('./Input/Letters/starting_letter.txt', 'r+') as file2:
+        content = file2.read()
+        for i in names:
+            with open(f'./Output/ReadyToSend/letter_for_{i}', 'w') as final:
+                letter = content.replace('[name]', i)
+                final.write(letter)
+                print(letter)
