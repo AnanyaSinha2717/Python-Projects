a2cdfb5b9430c9086a861d96492bcfb9c9e03886
AnanyaSinha2717 <ananyasinha2717@gmail.com>
2023-09-27 23:22:08 +0530

    moved files into correct folder

--- Weather_data/main.py
+++
@@ -1,9 +0,0 @@
-# with open('weather_data.csv') as file:
-#     data = file.readlines()
-#     print(data)
-
-import csv
-
-with open('weather_data.csv') as file:
-    data = csv.reader() #requires correct function
-print(data)
